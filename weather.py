1ce417790427a8b21894c452ba97ac813c3308f5
Vladimir Kotal <vlada@kotalovi.cz>
2022-01-05 16:36:21 +0100

    avoid KeyError exception by reusing the 'sensor' variable
    move the main loop to separate thread

--- weather.py
+++ weather.py
@@ -8,6 +8,7 @@ import ow
 import logging
 import time
 import sys
+import threading
 
 import board
 import adafruit_bmp280
@@ -24,15 +25,13 @@ sensor_names = {'21F723030000': TERASA,
 sensor_names_to_record = [KUCHYNE, TERASA]
 EXPOSED_PORT = 8111   # port to listen on for HTTP requests
 
+# It might take some significant time for measurements to be extracted from
+# OWFS so even with 1 second the loop will not be tight.
+sleep_seconds = 5
 
-def main():
-    # It might take some significant time for measurements to be extracted from
-    # OWFS so even with 1 second the loop will not be tight.
-    sleep_seconds = 5
 
+def sensor_loop():
     logger = logging.getLogger(__name__)
-    logger.setLevel(logging.INFO)
-    logger.info('Running')
 
     ow.init('localhost:4304')
     sensorlist = ow.Sensor('/').sensorList()
@@ -45,18 +44,13 @@ def main():
                               'Barometric pressure in hPa')}
 
     i2c = board.I2C()
-    sensor = adafruit_bmp280.Adafruit_BMP280_I2C(i2c)
-
-    # TODO: callibrate the sensor
-    # sensor.sea_level_pressure = 1013.25
-
-    logger.info("Starting HTTP server on port {}".format(EXPOSED_PORT))
-    start_http_server(EXPOSED_PORT)
+    bmp_sensor = adafruit_bmp280.Adafruit_BMP280_I2C(i2c)
 
     # TODO: move this to a thread (to avoid refused connections)
     while True:
-        pressure_val = sensor.pressure
+        pressure_val = bmp_sensor.pressure
         if pressure_val:
+            logger.info(f'pressure={pressure_val}')
             gauges[PRESSURE].set(pressure_val)
 
         logger.debug("sensors: {}".format(sensorlist))
@@ -86,6 +80,20 @@ def main():
 
         time.sleep(sleep_seconds)
 
+
+def main():
+    logger = logging.getLogger(__name__)
+    logger.setLevel(logging.INFO)
+    logger.info('Running')
+
+    # TODO: callibrate the sensor
+    # sensor.sea_level_pressure = 1013.25
+
+    logger.info("Starting HTTP server on port {}".format(EXPOSED_PORT))
+    start_http_server(EXPOSED_PORT)
+    thread = threading.Thread(target=sensor_loop, daemon=True)
+    thread.start()
+    thread.join()
 
 if __name__ == "__main__":
     logging.basicConfig()
