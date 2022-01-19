9bdaaa3391534373599c7100144d7fd5eb37d0d7
Vladimir Kotal <vlada@kotalovi.cz>
2022-01-19 20:08:23 +0100

    wrap the comment

--- weather.py
+++ weather.py
@@ -75,7 +75,8 @@ def sensor_loop(sleep_timeout, owfsdir, height):
         if scd4x_sensor:
             acquire_scd4x(gauges, scd4x_sensor)
 
-        # Acquire temperature before pressure so that pressure at sea level can be computed as soon as possible.
+        # Acquire temperature before pressure so that pressure at sea level
+        # can be computed as soon as possible.
         outside_temp = acquire_temperature(gauges, owfsdir)
 
         if bmp_sensor:
