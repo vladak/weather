45b1be73a491346940c5368a34bb6e62413ff510
Vladimir Kotal <vlada@kotalovi.cz>
2022-01-23 22:41:51 +0100

    apply black

--- test_alert.py
+++ test_alert.py
@@ -2,6 +2,6 @@ from alert import handle_grafana_alert
 
 
 def test_payload():
-    rule_name = 'foo'
-    payload = {'ruleName': rule_name, 'state': 'alerting'}
+    rule_name = "foo"
+    payload = {"ruleName": rule_name, "state": "alerting"}
     assert handle_grafana_alert(payload, rule_name + "bar", "foo.mp3")
